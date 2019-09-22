5f085dfa91c772b413df3a564c46496920ed9835
gabriel <gabrielbarreto.siqueira@gmail.com>
2019-09-22 17:27:02 -0300

    jogo de adivinhação

--- jogo de adivinhar.py
+++ jogo de adivinhar.py
@@ -1,10 +1,10 @@
 from random import randint
 from time import sleep
-computador = randint(0 , 5) #aqui o computador vai "pensar" num número de 0 a 5
+computador = randint(0 , 100) #aqui o computador vai "pensar" num número de 0 a 100
 print('-=-' * 20)
-print('Vou pensar em um número de 0 a 5, tente adivinhar')
+print('Vou pensar em um número de 0 a 100, tente adivinhar')
 print('-=-' * 20)
-jogador= int(input('Digite um número de 0 a 5: ')) #aqui o usuário vai advinhar o número que o computador "pensou"
+jogador= int(input('Digite um número de 0 a 100: ')) #aqui o usuário vai advinhar o número que o computador "pensou"
 print('PROCESSANDO...')
 sleep(3)
 if jogador == computador:
