7912068198b50983ce55f9b7c93288fe252e9220
gabriel <gabrielbarreto.siqueira@gmail.com>
2019-09-22 17:14:04 -0300

    Insirindo um programa no github

---
+++ jogo de adivinhar.py
@@ -0,0 +1,15 @@
+from random import randint
+from time import sleep
+computador = randint(0 , 5) #aqui o computador vai "pensar" num número de 0 a 5
+print('-=-' * 20)
+print('Vou pensar em um número de 0 a 5, tente adivinhar')
+print('-=-' * 20)
+jogador= int(input('Digite um número de 0 a 5: ')) #aqui o usuário vai advinhar o número que o computador "pensou"
+print('PROCESSANDO...')
+sleep(3)
+if jogador == computador:
+    print('PARABÉNS VOCÊ GANHOU!')
+else:
+    print('Que pena você perdeu, eu pensei em {} e não em {}'.format(computador , jogador))
+    print('BOA SORTE NA PROXIMA VEZ')
+
